4e8bbfca8503c3b58f8811bb79a2a56a093ab091
faisalsaificode <faisalsaifi.code@gmail.com>
2026-04-02 17:50:18 +0530

    Add custom domain

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/faisalsaifiportfolio/',
+  base: '/',
   server: { port: 3000, open: true }
 })
